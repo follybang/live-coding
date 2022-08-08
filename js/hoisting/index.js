2f278cc222d3b525e56ab1fb0e0d295c8b89269e
Bohdan <drabynko.b@gmail.com>
2022-08-08 17:35:39 +0300

    done

---
+++ js/hoisting/index.js
@@ -0,0 +1,32 @@
+// test();
+
+// console.log(price);
+
+// function test() {
+//   console.log('test');
+// }
+
+// var testArrowFunc = () => {
+//   console.log('testArrowFunc');
+// };
+
+// testArrowFunc();
+// var price = 100;
+
+// example 1
+
+// console.log(message);
+// var message = 'Hoisting is here!';
+
+// // example 2
+
+// var a = 55;
+// console.log(a);
+
+// if (a) {
+//   var a = 1;
+
+//   console.log(a);
+// }
+
+// console.log(a);
